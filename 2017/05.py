f9bfdc5241222d9407d554e4a46964c68824c3f1
Mark Street (MKT) <MKT@saxobank.com>
2017-12-07 12:17:56 +0000

    adding python solution for 2017.05

---
+++ 2017/05.py
@@ -0,0 +1,22 @@
+# --- Day 5: A Maze of Twisty Trampolines, All Alike ---
+
+with open("input/05.txt") as f:
+    data = f.read()[:-1] # drop newline
+    inst = map(int, data.split("\n"))
+    ptr = cnt = 0
+    while ptr < len(inst):
+      o = inst[ptr]
+      inst[ptr] += 1
+      ptr += o
+      cnt += 1
+    print cnt
+    #343467
+    inst = map(int, data.split("\n"))
+    ptr = cnt = 0
+    while ptr < len(inst):
+      o = inst[ptr]
+      inst[ptr] += -1 if o >= 3 else 1
+      ptr += o
+      cnt += 1
+    print cnt
+    #24774780
